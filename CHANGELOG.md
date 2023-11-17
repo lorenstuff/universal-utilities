1b5595d310a98ed80788e25e09036bbff1bb0447
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-11-17 13:06:10 -0500

    Adding localIpRanges array.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Changelog
 ## 1.1.0
-Added `splitStringIntoChunks` function.
+
+* Added `splitStringIntoChunks` function.
+* Added `localIpRanges` array.
 
 ## 1.0.0
 Initial release.
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@
 // Exports
 //
 
+export * from "./data/local-ip-ranges.js";
+
 export * from "./functions/escape-sql-like-string.js";
 export * from "./functions/split-string-into-chunks.js";
